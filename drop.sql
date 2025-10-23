d2602625d1cf1a0a6afb86b77a865cdd52fa57e9
AranOpazo <ara.opazo@duocuc.cl>
2025-10-23 15:43:54 -0300

    Agrego / por cada drop

--- drop.sql
+++ drop.sql
@@ -3,45 +3,53 @@ BEGIN
   EXECUTE IMMEDIATE 'DROP TRIGGER tgr_registra_pago';
 EXCEPTION WHEN OTHERS THEN NULL;
 END;
+/
 
 -- 1) Borrar package
 BEGIN
   EXECUTE IMMEDIATE 'DROP PACKAGE BODY pkg_admin_condominio';
 EXCEPTION WHEN OTHERS THEN NULL;
 END;
+/
 
 BEGIN
   EXECUTE IMMEDIATE 'DROP PACKAGE pkg_admin_condominio';
 EXCEPTION WHEN OTHERS THEN NULL;
 END;
+/
 
 -- 2) Borrar package de errores 
 BEGIN
   EXECUTE IMMEDIATE 'DROP PACKAGE BODY pkg_registro_errores';
 EXCEPTION WHEN OTHERS THEN NULL;
 END;
+/
 
 BEGIN
   EXECUTE IMMEDIATE 'DROP PACKAGE pkg_registro_errores';
 EXCEPTION WHEN OTHERS THEN NULL;
 END;
+/
 
 -- 3) Borrar tablas creadas
 BEGIN
   EXECUTE IMMEDIATE 'DROP TABLE registro_pagos';
 EXCEPTION WHEN OTHERS THEN NULL;
 END;
+/
 
 BEGIN
   EXECUTE IMMEDIATE 'DROP TABLE ERRORES_DETECTADOS';
 EXCEPTION WHEN OTHERS THEN NULL;
 END;
+/
 
 -- 4) Borrar secuencia
 BEGIN
   EXECUTE IMMEDIATE 'DROP SEQUENCE seq_errores_detectados';
 EXCEPTION WHEN OTHERS THEN NULL;
 END;
+/
 
 --------------------------------------
 --------------------------------------
